d73333b670384d25da9f7c5cf633b0b55f19b3bd
Cael Nguyen <cael.nguyen@avepoint.com>
2026-01-01 23:06:33 +0700

    config 2fa

---
+++ frontend/src/app/(main)/2fa/setup/Setup2FAClient.tsx
@@ -0,0 +1,18 @@
+"use client";
+import Setup2FAForm from "@/src/features/auth/components/Setup2FAForm";
+import { useSearchParams } from "next/navigation";
+
+export default function Setup2FAPage() {
+  const searchParams = useSearchParams();
+  const userId = Number(searchParams.get("userId"));
+
+  if (!userId) {
+    return <div>Invalid user</div>;
+  }
+
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <Setup2FAForm userId={userId} />
+    </div>
+  );
+}
