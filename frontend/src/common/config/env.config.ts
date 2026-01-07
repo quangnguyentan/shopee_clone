f46f2c9152cb71427c3fa95c36f62591e87c81cb
Cael Nguyen <cael.nguyen@avepoint.com>
2026-01-07 13:47:54 +0700

    fix(env) config env for production

--- frontend/src/common/config/env.config.ts
+++ frontend/src/common/config/env.config.ts
@@ -1,23 +1,22 @@
 import { z } from "zod";
 
 const envSchema = z.object({
-  NEXT_PUBLIC_RECAPTCHA_SITE_KEY: z.string().min(1, "Missing RECAPTCHA key"),
-  NEXT_PUBLIC_API_URL: z
-    .string()
-    .url("NEXT_PUBLIC_API_URL must be a valid URL"),
-  NEXT_PUBLIC_SOCKET_URL: z
-    .string()
-    .url("NEXT_PUBLIC_SOCKET_URL must be a valid URL"),
+  NEXT_PUBLIC_RECAPTCHA_SITE_KEY: z.string().min(1),
+  NEXT_PUBLIC_API_URL: z.string().url(),
+  NEXT_PUBLIC_SOCKET_URL: z.string().url(),
   NODE_ENV: z.enum(["development", "production", "test"]),
 });
 
 const _env = envSchema.parse({
   NEXT_PUBLIC_RECAPTCHA_SITE_KEY:
-    process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY ?? "",
+    process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY ||
+    (process.env.NODE_ENV === "development" ? "dev-key" : ""),
   NEXT_PUBLIC_API_URL:
-    process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:3000",
+    process.env.NEXT_PUBLIC_API_URL ||
+    (process.env.NODE_ENV === "development" ? "http://localhost:8080" : ""),
   NEXT_PUBLIC_SOCKET_URL:
-    process.env.NEXT_PUBLIC_SOCKET_URL ?? "http://localhost:3000",
+    process.env.NEXT_PUBLIC_SOCKET_URL ||
+    (process.env.NODE_ENV === "development" ? "http://localhost:8080" : ""),
   NODE_ENV: process.env.NODE_ENV ?? "development",
 });
 
