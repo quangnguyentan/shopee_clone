c1b59cd63d0b6c3dde1ff2c281db37bd0bc63e6f
Cael Nguyen <cael.nguyen@avepoint.com>
2026-01-07 13:40:37 +0700

    fix(env) config env for production

--- frontend/src/common/config/env.config.ts
+++ frontend/src/common/config/env.config.ts
@@ -1,13 +1,19 @@
 import { z } from "zod";
 
 const envSchema = z.object({
-  NEXT_PUBLIC_RECAPTCHA_SITE_KEY: z.string(),
+  NEXT_PUBLIC_RECAPTCHA_SITE_KEY: z.string().nonempty(),
   NEXT_PUBLIC_API_URL: z.string().url(),
   NEXT_PUBLIC_SOCKET_URL: z.string().url(),
   NODE_ENV: z.enum(["development", "production", "test"]),
 });
 
-const _env = envSchema.parse(process.env);
+const _env = envSchema.parse({
+  NEXT_PUBLIC_RECAPTCHA_SITE_KEY:
+    process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY ?? "",
+  NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL ?? "",
+  NEXT_PUBLIC_SOCKET_URL: process.env.NEXT_PUBLIC_SOCKET_URL ?? "",
+  NODE_ENV: process.env.NODE_ENV ?? "development",
+});
 
 export const env = {
   ..._env,
