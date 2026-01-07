ed4cb90c5aecdc0ee72e5c824d616632b8da5853
Cael Nguyen <cael.nguyen@avepoint.com>
2026-01-07 14:01:32 +0700

    fix(env) config env for production

--- frontend/src/common/config/env.config.ts
+++ frontend/src/common/config/env.config.ts
@@ -1,14 +1,18 @@
 import { z } from "zod";
 
 const envSchema = z.object({
-  NEXT_PUBLIC_API_URL: z.string().url("Invalid NEXT_PUBLIC_API_URL"),
-  NEXT_PUBLIC_SOCKET_URL: z.string().url("Invalid NEXT_PUBLIC_SOCKET_URL"),
+  NEXT_PUBLIC_API_URL: z.string().url(),
+  NEXT_PUBLIC_SOCKET_URL: z.string().url(),
   NODE_ENV: z.enum(["development", "production", "test"]),
 });
 
 const _env = envSchema.parse({
-  NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
-  NEXT_PUBLIC_SOCKET_URL: process.env.NEXT_PUBLIC_SOCKET_URL,
+  NEXT_PUBLIC_API_URL:
+    process.env.NEXT_PUBLIC_API_URL ??
+    (process.env.NODE_ENV === "development" ? "http://localhost:8080" : ""),
+  NEXT_PUBLIC_SOCKET_URL:
+    process.env.NEXT_PUBLIC_SOCKET_URL ??
+    (process.env.NODE_ENV === "development" ? "http://localhost:8080" : ""),
   NODE_ENV: process.env.NODE_ENV ?? "development",
 });
 
