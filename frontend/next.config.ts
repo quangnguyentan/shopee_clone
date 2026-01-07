0d696124057863b041d34326f31a523453f9bb13
Cael Nguyen <cael.nguyen@avepoint.com>
2026-01-07 15:36:20 +0700

    fix(env) config env for production

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,19 +1,16 @@
+import type { NextConfig } from "next";
+
 const repo = "shopee_clone";
 const isProd = process.env.NODE_ENV === "production";
 
-const nextConfig = {
-  output: "export",
-  basePath: isProd ? `/${repo}` : "",
-  assetPrefix: isProd ? `/${repo}/` : "",
+const nextConfig: NextConfig = {
+  output: isProd ? "export" : undefined,
 
-  images: { unoptimized: true },
+  basePath: isProd ? `/${repo}` : undefined,
+  assetPrefix: isProd ? `/${repo}/` : undefined,
 
-  reactStrictMode: true,
-
-  env: {
-    NEXT_PUBLIC_RECAPTCHA_SITE_KEY: process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY,
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
-    NEXT_PUBLIC_SOCKET_URL: process.env.NEXT_PUBLIC_SOCKET_URL,
+  images: {
+    unoptimized: true,
   },
 };
 
