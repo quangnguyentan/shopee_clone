841525ca8bf35aea4847cf34fdbef9ce7deb353b
Cael Nguyen <cael.nguyen@avepoint.com>
2026-01-07 14:14:56 +0700

    fix(env) config env for production

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,14 +1,13 @@
 import type { NextConfig } from "next";
 import { env } from "./src/common/config/env.config";
+
 const repo = "shopee_clone";
 
 const nextConfig: NextConfig = {
-  output: env.isProd ? "export" : undefined,
   basePath: env.isProd ? `/${repo}` : undefined,
   assetPrefix: env.isProd ? `/${repo}/` : undefined,
-  images: {
-    unoptimized: true,
-  },
+  images: { unoptimized: true },
+  output: undefined,
 };
 
 export default nextConfig;
--- frontend/src/common/config/env.config.ts
+++ frontend/src/common/config/env.config.ts
@@ -1,4 +1,17 @@
+// src/common/config/env.config.ts
 import { z } from "zod";
+import { config as dotenvConfig } from "dotenv";
+import path from "path";
+
+// Load env file theo NODE_ENV
+dotenvConfig({
+  path: path.resolve(
+    process.cwd(),
+    process.env.NODE_ENV === "production"
+      ? ".env.production.local"
+      : ".env.development.local"
+  ),
+});
 
 const envSchema = z.object({
   NEXT_PUBLIC_RECAPTCHA_SITE_KEY: z.string().min(1, "Missing RECAPTCHA key"),
@@ -8,9 +21,9 @@ const envSchema = z.object({
 });
 
 const _env = envSchema.parse({
-  NEXT_PUBLIC_RECAPTCHA_SITE_KEY: process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY!,
-  NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL!,
-  NEXT_PUBLIC_SOCKET_URL: process.env.NEXT_PUBLIC_SOCKET_URL!,
+  NEXT_PUBLIC_RECAPTCHA_SITE_KEY: process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY,
+  NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  NEXT_PUBLIC_SOCKET_URL: process.env.NEXT_PUBLIC_SOCKET_URL,
   NODE_ENV: process.env.NODE_ENV ?? "development",
 });
 
