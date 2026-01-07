038cd54cbcbdd7bb8baf1142d19f7d9cb2869f30
Cael Nguyen <cael.nguyen@avepoint.com>
2026-01-07 14:17:07 +0700

    fix(env) config env for production

--- frontend/src/common/config/env.config.ts
+++ frontend/src/common/config/env.config.ts
@@ -1,22 +1,9 @@
-// src/common/config/env.config.ts
 import { z } from "zod";
-import { config as dotenvConfig } from "dotenv";
-import path from "path";
-
-// Load env file theo NODE_ENV
-dotenvConfig({
-  path: path.resolve(
-    process.cwd(),
-    process.env.NODE_ENV === "production"
-      ? ".env.production.local"
-      : ".env.development.local"
-  ),
-});
 
 const envSchema = z.object({
-  NEXT_PUBLIC_RECAPTCHA_SITE_KEY: z.string().min(1, "Missing RECAPTCHA key"),
-  NEXT_PUBLIC_API_URL: z.string().url("Invalid NEXT_PUBLIC_API_URL"),
-  NEXT_PUBLIC_SOCKET_URL: z.string().url("Invalid NEXT_PUBLIC_SOCKET_URL"),
+  NEXT_PUBLIC_RECAPTCHA_SITE_KEY: z.string().min(1),
+  NEXT_PUBLIC_API_URL: z.string().url(),
+  NEXT_PUBLIC_SOCKET_URL: z.string().url(),
   NODE_ENV: z.enum(["development", "production", "test"]),
 });
 
