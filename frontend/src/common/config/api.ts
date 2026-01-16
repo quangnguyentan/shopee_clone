d44036f87356e525f13cef0b9de412e60aea7cbf
Cael Nguyen <cael.nguyen@avepoint.com>
2026-01-16 09:22:42 +0700

    fix(api refresh) custom api refresh for buyer UI

--- frontend/src/common/config/api.ts
+++ frontend/src/common/config/api.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { getEnv } from "./env.client";
+import { AUTH_SCOPE } from "../constants";
 
 const env = getEnv();
 
@@ -7,6 +8,8 @@ export const api = axios.create({
   baseURL: env.NEXT_PUBLIC_API_URL,
   withCredentials: true,
 });
+
+api.defaults.headers.common["x-auth-scope"] = AUTH_SCOPE;
 
 declare module "axios" {
   export interface AxiosRequestConfig {
